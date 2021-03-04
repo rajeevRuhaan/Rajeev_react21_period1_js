d535f6accdf2d684a7c25c92bc4bd65e28af9a6e
Rajeev <s2100155@edu.bc.fi>
2021-03-04 04:24:12 +0200

    myJson printed after fetch users done

--- Lesson5_AsyncPromise/script.js
+++ Lesson5_AsyncPromise/script.js
@@ -8,10 +8,11 @@ fetch("https://randomuser.me/api/?results=100") // Promise
   .then((json) => {
     users = json.results;
     console.log(users);
+    console.log(myJSON); //print after the users has been fetch
   })
   .catch((err) => {
     console.log("An err has occured while trying to fetch random users");
   });
+
 var myObj = { name: "John", age: 31, city: "New York" };
 var myJSON = JSON.stringify(myObj);
-window.location = "demo_json.php?x=" + myJSON;
