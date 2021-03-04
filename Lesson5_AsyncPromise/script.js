2b38515728123fc81afd61c7f3a2944aee5f3ac5
Rajeev <s2100155@edu.bc.fi>
2021-03-04 20:32:41 +0200

    using fetch api to get pokemon name

--- Lesson5_AsyncPromise/script.js
+++ Lesson5_AsyncPromise/script.js
@@ -1,18 +1,27 @@
 (function () {
-  window.addEventListener("DOMContentLoaded", (event) => {});
-});
-console.log("hello");
-let users = [];
-fetch("https://randomuser.me/api/?results=100") // Promise
-  .then((resp) => resp.json()) // unsorted array of user object
-  .then((json) => {
-    users = json.results;
-    console.log(users);
-    console.log(myJSON); //print after the users has been fetch
-  })
-  .catch((err) => {
-    console.log("An err has occured while trying to fetch random users");
-  });
+  window.addEventListener("DOMContentLoaded", executeScript);
+  let userNameClicked = document.getElementsByTagName("a");
 
-var myObj = { name: "John", age: 31, city: "New York" };
-var myJSON = JSON.stringify(myObj);
+  function executeScript() {
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000") // Promise
+      .then((resp) => resp.json()) //
+      .then((json) => {
+        // users = json.results;
+        // console.log(users);
+        json.results.forEach(addUser);
+      });
+  }
+  /*  .catch((err) => {
+        console.log("err", err);
+        console.log("An err has occured while trying to fetch random users");
+      }); */
+
+  function addUser(user) {
+    const newEl = document.createElement("div");
+    newEl.className = "user";
+    newEl.innerHTML = `<a href="#">${user.name}</a>`;
+    document.getElementById("user-List").appendChild(newEl);
+  }
+
+  usernameClicked.addEventListener();
+})();
