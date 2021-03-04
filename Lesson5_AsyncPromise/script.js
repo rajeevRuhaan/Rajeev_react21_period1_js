8f7b0fb224cf68c3f49acdd4e6eacd45d6248594
Rajeev <s2100155@edu.bc.fi>
2021-03-04 03:50:52 +0200

    someting added

--- Lesson5_AsyncPromise/script.js
+++ Lesson5_AsyncPromise/script.js
@@ -12,3 +12,6 @@ fetch("https://randomuser.me/api/?results=100") // Promise
   .catch((err) => {
     console.log("An err has occured while trying to fetch random users");
   });
+var myObj = { name: "John", age: 31, city: "New York" };
+var myJSON = JSON.stringify(myObj);
+window.location = "demo_json.php?x=" + myJSON;
