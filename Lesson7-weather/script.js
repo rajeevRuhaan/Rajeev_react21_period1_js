0c5f6846ab60c9f9cf0089fb7e3dede953abb5b6
Rajeev <s2100155@edu.bc.fi>
2021-03-10 12:06:53 +0200

    script with element initial

---
+++ Lesson7-weather/script.js
@@ -0,0 +1,10 @@
+let notificationElement = document.querySelector(".notification");
+let iconElement = document.querySelector(".ewather-icon");
+let tempElement = document.querySelector(".temp-value p");
+let descElement = document.querySelector(".temp-description p");
+let locationElement = document.querySelector(".location p");
+
+const weather = {};
+weather.temperature = {
+  unit: "Celsius",
+};
